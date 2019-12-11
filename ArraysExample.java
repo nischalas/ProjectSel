8f535b0874e6b4eea8e460c6ddea3434ffea3b6f
nischalas <nichusimhadri@gmail.com>
2019-12-11 16:15:54 -0500

    first progr has been committed

---
+++ ArraysExample.java
@@ -0,0 +1,29 @@
+package seleniumproject123;
+
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class ArraysExample {
+
+	public static void main(String[] args) {
+
+		
+//		
+//		int[] a= new int[3];
+//				System.out.println(a);
+//				
+//				System.out.println(a[1]);
+//
+//		
+		
+		int[][] x= new int[6][3];
+		System.out.println(x.length);
+		
+		System.out.println(x[0].length);
+
+		
+		
+	ChromeOptions opt =new ChromeOptions();
+	opt.addArguments("--disable-notifications");
+	}
+
+}
